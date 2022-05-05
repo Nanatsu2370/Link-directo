6ad3351481177bc432dd1fba474cda1d1297645b
Nanatsu2370 <94955661+Nanatsu2370@users.noreply.github.com>
2022-05-05 06:42:13 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
-api_hash = "0c780f6db3124fafbdc3dcde437e6f21"
-api_id = 16536294
-bot_token = "5303873066:AAHoGX4eiQHmmZrOMzM47ROWcQ47lrcfSxY"
-bot_admin_group = -1001505005021
+api_hash = ""
+api_id = 
+bot_token = ""
+bot_admin_group = 
